ca38132033675d2c011d130c4781e3db3fca2ce2
my name <myemail@email.com>
2017-04-26 10:32:25 +0800

    first commit

---
+++ lifeVC/src/app.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ele-header></ele-header>
+    <div class="tab">
+      <div class="tab-item">
+        <a v-link="{path: '/goods'}">商品</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path: '/ratings'}">评价</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path: '/seller'}">商家</a>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header.vue'
+
+  export default {
+
+    components: {
+      'ele-header': header
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .tab
+    .tab-item
+      display: inline-block
+      .v-link-active
+        color: #ff0000
+</style>
